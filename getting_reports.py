89d67c54850b5f323239fb59ccd05a1f6a702584
Zohaib <zohaibshahid520@gmail.com>
2024-03-12 04:38:04 -0600

    Updating Files

--- getting_reports.py
+++ getting_reports.py
@@ -1,37 +1,91 @@
+import os
 import requests
 import json
 
-# Define the endpoint URL
-url = "https://api.timelyapp.com/1.1/918926/reports/filter"
+def fetch_and_save_reports(since, until):
 
-# Define the headers
-headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-    "Authorization": "Bearer FfMh-1CGi4aWsu376jpMUOX6R1GVQbAGHxAY6y-eBMM"
-}
+    data_folder = 'data'
+    if not os.path.exists(data_folder):
+        os.makedirs(data_folder)
 
-# Define the request parameters
-data = {
-    "since": "2024-02-26",  # Start Date
-    "until": "2024-02-26",  # End Date
-    "group_by": ["clients", "users", "labels", "days"],
-    "scope": "events"
-}
+    # Create a folder with the specified date if it doesn't exist
+    date_folder = os.path.join(data_folder, since)
+    if not os.path.exists(date_folder):
+        os.makedirs(date_folder)
 
-# Make the POST request
-response = requests.post(url, headers=headers, data=json.dumps(data))
+    # Define the endpoint URL
+    url = "https://api.timelyapp.com/1.1/918926/reports/filter"
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Convert the response to JSON
-    reports = response.json()
-    # Specify the path where you want to save the JSON file
-    file_path = 'reports_02_26.json'
+    # Define the headers
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer FfMh-1CGi4aWsu376jpMUOX6R1GVQbAGHxAY6y-eBMM"
+    }
 
-    # Save the JSON data to a file
-    with open(file_path, 'w') as json_file:
-        json.dump(reports, json_file)
-    # print("Reports fetched successfully:", reports)
-else:
-    print(f"Failed to fetch reports. Status code: {response.status_code}")
+    # Define the request parameters for the first API request
+    data = {
+        "since": since,
+        "until": until,
+    }
+
+    # Make the POST request for the first API request
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Convert the response to JSON
+        reports = response.json()
+
+        # Initialize a list to store client_id and project_id pairs
+        client_project_pairs = []
+
+        # Iterate over the clients and projects in the response
+        for client in reports.get("clients", []):
+            client_id = client.get("id")
+            client_name = client.get("name")
+
+            for project in client.get("projects", []):
+                project_id = project.get("id")
+                project_name = project.get("name")
+
+                # Add client_id and project_id pair to the list
+                client_project_pairs.append((client_id,project_id))
+
+        # Iterate over client_id and project_id pairs
+        for project_id in client_project_pairs:
+            # Make the API request with client_id and project_id
+            data_second_request = {
+                "since": since,
+                "until": until,
+                "project_ids": [project_id],
+                "group_by": ["clients", "users", "labels", "days"],
+                "scope": "events"
+            }
+
+            response_second_request = requests.post(url, headers=headers, data=json.dumps(data_second_request))
+
+            if response_second_request.status_code == 200:
+                reports_second_request = response_second_request.json()
+
+                # Specify the path where you want to save the JSON file for this client_id and project_id pair
+                file_path_second_request = os.path.join(date_folder, f'reports_{since}_{until}_{client_id}_{project_id}.json')
+
+                # Save the JSON data to a file for this client_id and project_id pair
+                with open(file_path_second_request, 'w') as json_file_second_request:
+                    json.dump(reports_second_request, json_file_second_request)
+            else:
+                print(f"Failed to fetch reports for client_id '{client_id}', project_id '{project_id}'. "
+                      f"Status code: {response_second_request.status_code}")
+
+        # Specify the path where you want to save the JSON file for the first request
+        file_path_first_request = os.path.join(date_folder, f'reports_{since}_{until}_all.json')
+
+        # Save the JSON data to a file for the first request
+        with open(file_path_first_request, 'w') as json_file_first_request:
+            json.dump(reports, json_file_first_request)
+    else:
+        print(f"Failed to fetch reports. Status code: {response.status_code}")
+
+
+fetch_and_save_reports("2024-02-26", "2024-02-26")
--- storing_csv_files.py
+++ storing_csv_files.py
@@ -1,6 +1,6 @@
+import os
 import json
 import csv
-
 
 def extract_label_name(label_id, labels_data):
     for label in labels_data:
@@ -8,142 +8,143 @@ def extract_label_name(label_id, labels_data):
             return label['name']
     return None
 
-def extract_and_store_csv(json_filename, csv_filename, labels_filename):
-    # Read JSON data from the 'reports' file
+def extract_and_store_csv(json_folder, csv_filename, labels_filename):
     with open(labels_filename, 'r') as labels_file:
         labels_data = json.load(labels_file)['labels']
 
-    with open(json_filename, 'r') as json_file:
-        data = json.load(json_file)
-
-    # Extracted data storage
     extracted_data = []
 
-    # Iterate through each JSON object in the list
-    for entry in data:
-        Client = entry['project']['client']['name']
-        Client_ID = entry['project']['client']['id']
-        Project = entry['project']['name']
-        Project_ID = entry['project']['id']
-        Hour_Date = entry['day']
-        user_name = entry['user']['name']
-        user_id = entry['user']['id']
-        user_email = entry['user']['email']
-        billable_hours = entry['duration']['total_hours']
-        logged_money = entry['cost']['amount']
-        hour_billed_status = "Yes" if entry['billed'] else "No"
-        hour_notes = entry['note']
-        external_id = ','.join(map(str, entry['external_link_ids']))
-        budget_type = entry['project']['budget_type']
-        project_description = entry['project']['description']
+    csv_exists = os.path.exists(csv_filename) and os.path.getsize(csv_filename) > 0
 
-        # Extract label_ids and required_label_ids
-        label_ids = entry['label_ids']
-        required_label_ids = entry['project']['required_label_ids']
-
-        # Calculate the difference between label_ids and required_label_ids
-        label_id_difference = list(set(label_ids) - set(required_label_ids))
-
-        # Fetch label names based on label IDs in 'Label ID Difference'
-        label_names = [extract_label_name(int(label_id), labels_data) for label_id in label_id_difference]
-
-        # Convert label names to a comma-separated string
-        label_names_str = ','.join(filter(None, label_names))
-
-        # Extract timestamps dynamically
-        timestamps = entry.get('timestamps', [])
-        for timestamp in timestamps:
-            from_time = timestamp['from'][11:16]  # Extracts only the hour and minute
-            to_time = timestamp['to'][11:16]  # Extracts only the hour and minute
-
-            # Append the extracted data to the list
-            extracted_data.append({
-                'Client': Client,
-                'Client ID' : Client_ID,
-                'Project': Project,
-                'Project ID' : Project_ID,
-                'Hour Date': Hour_Date,
-                'Name' : user_name,
-                'ID' : user_id,
-                'Billable Hours' : billable_hours,
-                'Logged Money': logged_money,
-                'Hour Tags': label_names_str,
-                'Hour Billed Status' : hour_billed_status,
-                'Hour Note': hour_notes,
-                'Hour From': from_time,
-                'Hour To': to_time,
-                'Teams': None,
-                'External ID': external_id,
-                'Logged Hours' : None,
-                'Non-billable Hours': None,
-                'Budget Type': budget_type,
-                'Budget Interval' : None,
-                'Budget Total' : None,
-                'Budget Spent' : None,
-                'Budget Spent (%)' : None,
-                'Budget Remaining' : None,
-                'Budget Remaining (%)' : None,
-                'Logged Cost': None,
-                'Planned Hours': 0,
-                'Planned Money (€)' : 0,
-                'Planned Cost (€)' : None,
-                'Email' : user_email,
-                'Project Description' : project_description
-            })
+    with open(csv_filename, 'a', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['Client', 'Client ID', 'Project', 'Project ID', 'Hour Date', 'Name', 'ID', 'Billable Hours', 'Logged Money',
+                      'Hour Tags', 'Hour Billed Status', 'Hour Note', 'Hour From', 'Hour To', 'Teams', 'External ID',
+                      'Logged Hours', 'Non-billable Hours', 'Budget Type', 'Budget Interval', 'Budget Total',
+                      'Budget Spent', 'Budget Spent (%)', 'Budget Remaining', 'Budget Remaining (%)', 'Logged Cost',
+                      'Planned Hours', 'Planned Money (€)', 'Planned Cost (€)', 'Email', 'Project Description']
         
-        # If no timestamps, add a row with 'From' and 'To' as None
-        if not timestamps:
-            extracted_data.append({
-                'Client': Client,
-                'Client ID' : Client_ID,
-                'Project': Project,
-                'Project ID' : Project_ID,
-                'Hour Date': Hour_Date,
-                'Name' : user_name,
-                'ID' : user_id,
-                'Billable Hours' : billable_hours,
-                'Logged Money': logged_money,
-                'Hour Tags': label_names_str,
-                'Hour Billed Status' : hour_billed_status,
-                'Hour Note': hour_notes,
-                'Hour From': None,
-                'Hour To': None,
-                'Teams': None,
-                'External ID': external_id,
-                'Logged Hours' : None,
-                'Non-billable Hours': None,
-                'Budget Type': budget_type,
-                'Budget Interval' : None,
-                'Budget Total' : None,
-                'Budget Spent' : None,
-                'Budget Spent (%)' : None,
-                'Budget Remaining' : None,
-                'Budget Remaining (%)' : None,
-                'Logged Cost': None,
-                'Planned Hours': 0,
-                'Planned Money (€)' : 0,
-                'Planned Cost (€)' : None,
-                'Email' : user_email,
-                'Project Description' : project_description
-            })
-
-    # Write the extracted data to a CSV file
-    with open(csv_filename, 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['Client', 'Client ID', 'Project','Project ID', 'Hour Date', 'Name', 'ID', 'Billable Hours', 'Logged Money', 'Hour Tags',
-                'Hour Billed Status', 'Hour Note', 'Hour From', 'Hour To', 'Teams', 'External ID',
-                'Logged Hours', 'Non-billable Hours', 'Budget Type', 'Budget Interval', 'Budget Total',
-                'Budget Spent', 'Budget Spent (%)', 'Budget Remaining', 'Budget Remaining (%)', 'Logged Cost',
-                'Planned Hours', 'Planned Money (€)', 'Planned Cost (€)', 'Email', 'Project Description']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-        # Write the header
-        writer.writeheader()
+        if not csv_exists:
+            writer.writeheader()
 
-        # Write the data
+        # Iterate through each JSON file in the specified folder
+        for filename in os.listdir(json_folder):
+            if filename.endswith(".json") and filename != 'reports_2024-02-26_2024-02-26_all.json':
+                json_filepath = os.path.join(json_folder, filename)
+
+                with open(json_filepath, 'r') as json_file:
+                    data = json.load(json_file)
+
+                for entry in data:
+                    Client = entry['project']['client']['name']
+                    Client_ID = entry['project']['client']['id']
+                    Project = entry['project']['name']
+                    Project_ID = entry['project']['id']
+                    Hour_Date = entry['day']
+                    user_name = entry['user']['name']
+                    user_id = entry['user']['id']
+                    user_email = entry['user']['email']
+                    billable_hours = round(entry['duration']['total_hours'], 2)
+                    logged_money = entry['cost']['amount']
+                    hour_billed_status = "Yes" if entry['billed'] else "No"
+                    hour_notes = entry['note']
+                    external_id = ','.join(map(str, entry['external_link_ids']))
+                    budget_type = entry['project']['budget_type']
+                    project_description = entry['project']['description']
+
+                    # Extract label_ids and required_label_ids
+                    label_ids = entry['label_ids']
+                    required_label_ids = entry['project']['required_label_ids']
+
+                    # Calculate the difference between label_ids and required_label_ids
+                    label_id_difference = list(set(label_ids) - set(required_label_ids))
+
+                    # Fetch label names based on label IDs in 'Label ID Difference'
+                    label_names = [extract_label_name(int(label_id), labels_data) for label_id in label_id_difference]
+
+                    # Convert label names to a comma-separated string
+                    label_names_str = ','.join(filter(None, label_names))
+
+                    # Extract timestamps dynamically
+                    timestamps = entry.get('timestamps', [])
+                    for timestamp in timestamps:
+                        from_time = timestamp['from'][11:16]  # Extracts only the hour and minute
+                        to_time = timestamp['to'][11:16]  # Extracts only the hour and minute
+
+                        extracted_data.append({
+                            'Client': Client,
+                            'Client ID' : Client_ID,
+                            'Project': Project,
+                            'Project ID' : Project_ID,
+                            'Hour Date': Hour_Date,
+                            'Name' : user_name,
+                            'ID' : user_id,
+                            'Billable Hours' : billable_hours,
+                            'Logged Money': logged_money,
+                            'Hour Tags': label_names_str,
+                            'Hour Billed Status' : hour_billed_status,
+                            'Hour Note': hour_notes,
+                            'Hour From': from_time,
+                            'Hour To': to_time,
+                            'Teams': None,
+                            'External ID': external_id,
+                            'Logged Hours' : None,
+                            'Non-billable Hours': None,
+                            'Budget Type': budget_type,
+                            'Budget Interval' : None,
+                            'Budget Total' : None,
+                            'Budget Spent' : None,
+                            'Budget Spent (%)' : None,
+                            'Budget Remaining' : None,
+                            'Budget Remaining (%)' : None,
+                            'Logged Cost': None,
+                            'Planned Hours': 0,
+                            'Planned Money (€)' : 0,
+                            'Planned Cost (€)' : None,
+                            'Email' : user_email,
+                            'Project Description' : project_description
+                        })
+
+                    if not timestamps:
+                        extracted_data.append({
+                            'Client': Client,
+                            'Client ID' : Client_ID,
+                            'Project': Project,
+                            'Project ID' : Project_ID,
+                            'Hour Date': Hour_Date,
+                            'Name' : user_name,
+                            'ID' : user_id,
+                            'Billable Hours' : billable_hours,
+                            'Logged Money': logged_money,
+                            'Hour Tags': label_names_str,
+                            'Hour Billed Status' : hour_billed_status,
+                            'Hour Note': hour_notes,
+                            'Hour From': None,
+                            'Hour To': None,
+                            'Teams': None,
+                            'External ID': external_id,
+                            'Logged Hours' : None,
+                            'Non-billable Hours': None,
+                            'Budget Type': budget_type,
+                            'Budget Interval' : None,
+                            'Budget Total' : None,
+                            'Budget Spent' : None,
+                            'Budget Spent (%)' : None,
+                            'Budget Remaining' : None,
+                            'Budget Remaining (%)' : None,
+                            'Logged Cost': None,
+                            'Planned Hours': 0,
+                            'Planned Money (€)' : 0,
+                            'Planned Cost (€)' : None,
+                            'Email' : user_email,
+                            'Project Description' : project_description
+                        })
+
         writer.writerows(extracted_data)
 
-# Example usage:
-json_filename = 'reports_02_26.json'
-csv_filename = 'extracted_data_02_26.csv'
-labels_filename = 'reports_overall_no_filters.json'
-extract_and_store_csv(json_filename, csv_filename, labels_filename)
+json_folder = 'data/2024-02-26' 
+csv_filename = 'extracted_data__date.csv'
+labels_filename = 'data/2024-02-26/reports_2024-02-26_2024-02-26_all.json' 
+extract_and_store_csv(json_folder, csv_filename, labels_filename)
+
